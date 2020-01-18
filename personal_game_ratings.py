761f7048233a263695bee10f9ee487fceae2fa01
Naught Robot <naughtrobot@pm.me>
2020-01-18 12:29:49 -0700

    This is where it all begins...

---
+++ personal_game_ratings.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# encoding=utf8
+"""Personal BGG Ratings."""
+
+import argparse
+import sys
+from functools import cmp_to_key
+
+import requests
+import xmltodict
+
+sys.getdefaultencoding()
+
+RETRY = 0
+
+
+def get_args():
+    """Gather command line arguments or display help."""
+    parser = argparse.ArgumentParser(description='BGG Game Rankings',
+                                     add_help=False)
+    parser.add_argument('-h', '--help', action='help',
+                        default=argparse.SUPPRESS,
+                        help='Show this help message and exit.')
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='version',
+        version='%(prog)s 2.0.1',
+        help="Show program's version \
+                        number")
+    parser.add_argument('-u', '--user', help='BGG username',
+                        required=True, metavar='')
+    parser.add_argument('-c', '--count', help='Number of results',
+                        required=False, metavar='')
+    return parser.parse_args()
+
+
+def request_data(url):
+    """Request data from boardgamegeek."""
+    data = requests.get(url)
+    return data.content
+
+
+def weighted_average(rating, mean, plays):
+    """Weighted average for game ratings."""
+    """
+    WR = (v/(v+m)*R + (m/(v+m))* C
+    R = average rating for the game.
+    v = number of plays of the game.
+    m = minimum number of plays of the game.
+    C = mean rating for the game.
+    """
+    rating, mean, plays = float(rating), float(mean), float(plays)
+    minimum_plays = 10
+    rating = (plays / (plays + minimum_plays)) * rating \
+        + (minimum_plays / (plays + minimum_plays)) * mean
+    return rating
+
+
+def multikeysort(items, columns):
+    """Sort dictionary based on multiple keys."""
+    from operator import itemgetter
+    comparers = [((itemgetter(col[1:].strip()), 1) if col.startswith('-') else
+                  (itemgetter(col.strip()), -1)) for col in columns]
+
+    def comparer(left, right):
+        for _fn, mult in comparers:
+            result = ((_fn(left) > _fn(right)) - (_fn(left) < _fn(right)))
+            if result:
+                return mult * result
+        return None
+
+    return sorted(items, key=cmp_to_key(comparer))
+
+
+def get_collection(username):
+    """Get user's collection from BGG."""
+    baseurl = 'https://www.boardgamegeek.com/xmlapi2/'
+    url = baseurl + ('collection?username={}&own=1'
+                     '&rated=1&played=1&stats=1').format(username)
+    data = request_data(url)
+    doc = xmltodict.parse(data)
+    collection = []
+    try:
+        for game in doc['items']['item']:
+            title = game['name']['#text'].encode('utf-8').strip()
+            player_rating = game['stats']['rating']['@value']
+            mean_rating = game['stats']['rating']['average']['@value']
+            plays = game['numplays']
+            rating = weighted_average(player_rating, mean_rating, plays)
+            collection.append({'name': title, 'rating': rating,
+                               'plays': plays})
+    except KeyError:
+        global RETRY
+        if RETRY < 5:
+            RETRY += 1
+            get_collection(username)
+        else:
+            sys.exit(1)
+    return collection
+
+
+def display_top_games(collection, count):
+    """Display top games based on ratings then number of plays."""
+    collection = multikeysort(collection, ['rating', 'name'])
+    print("{0:<5}{1:<7}{2:<7}{3:<100}".format('Rank', 'Rating', 'Plays',
+                                              'Game'))
+    rank = 1
+    for game in collection:
+        print(
+            "{0:<5d}{1:<7.4f}{2:<7d}{3:<100s}".format(
+                rank, float(
+                    game['rating']), int(
+                    game['plays']), str(
+                    game['name']).strip('b').strip('\'').strip('\"')))
+        if count:
+            if rank >= int(count):
+                break
+            else:
+                rank += 1
+        else:
+            rank += 1
+
+
+if __name__ == "__main__":
+    ARGS = get_args()
+    display_top_games(get_collection(ARGS.user), ARGS.count)
