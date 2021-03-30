2b488bed31eaf3248fdab044e47d50d17aa1acf3
NaughtRobot <naughtrobot@pm.me>
2021-03-30 11:42:03 -0600

    Add Hot Board Games List

--- BoardgameHotness/boardgame_hotness.py
+++ BoardgameHotness/boardgame_hotness.py
@@ -2,12 +2,30 @@
 # encoding=utf8
 """Boardgame Geek Hot Games List."""
 
-class Boardgame:
-    """Boardgame Class."""
+import requests
+import xmltodict
 
-    def __init__ (self, game_id, title, rank, year_published):
-        """Create Variables."""
-        self.game_id = game_id
-        self.title  = title
-        self.rank = rank
-        self.year_published = year_published
+
+def request_data(url):
+    """Request data from boardgamegeek."""
+    data = requests.get(url)
+    return data.content
+
+def get_hotness():
+    """Return current list of hot games on Board Game Geek."""
+    url = "https://www.boardgamegeek.com/xmlapi2/hot?boardgame"
+    the_list = xmltodict.parse(request_data(url))
+    print("{0:<6s}{1:<7s}{2:100s}".format("Rank","Year","Title"))
+    count = 1
+    for game in the_list['items']['item']:
+        if count < 11:
+            print("{0:<6s}{1:<7s}{2:100s}".format(game['@rank'],
+                   game['yearpublished']['@value'],
+                   game['name']['@value']))
+            count += 1
+        else:
+            break
+
+if __name__ == "__main__":
+    get_hotness()
+       
