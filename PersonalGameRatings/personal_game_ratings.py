6b86b66b09d1800911f1a2d834bf693ba88c566a
NaughtRobot <naughtrobot@pm.me>
2021-09-27 21:38:08 -0600

    Converted Ranking to Use Bayesian Average

--- PersonalGameRatings/personal_game_ratings.py
+++ PersonalGameRatings/personal_game_ratings.py
@@ -26,12 +26,14 @@ def get_args():
         '-v',
         '--version',
         action='version',
-        version='%(prog)s 3.2.0',
+        version='%(prog)s 4.0.0',
         help="Show program's version number")
     parser.add_argument('-u', '--user', help='BGG username',
                         required=True, metavar='')
     parser.add_argument('-c', '--count', help='Number of results',
                         required=False, metavar='')
+    parser.add_argument('-d', '--detailed', help='Detailed output',
+                        required=False, action='store_true')
     return parser.parse_args()
 
 
@@ -46,20 +48,19 @@ def request_data(url):
     return data.text
 
 
-def weighted_average(rating, mean, plays):
-    """Weighted average for game ratings.
+def bayesian_average(rating, mean, plays):
+    """Bayesian average for game based on number of plays.
 
-    WR = (v/(v+m)*R + (m/(v+m))* C
+    BA = R*(p+m)*M/(p+m)
     R = rating for the game.
-    v = number of plays of the game.
+    p = number of plays of the game.
     m = minimum number of plays of the game.
-    C = mean rating for all games rated and owned.
+    M = mean rating for all games rated and owned.
     """
+    minimum_plays = 5
     rating, mean, plays = float(rating), float(mean), float(plays)
-    minimum_plays = 25
-    rating = (plays / (plays + minimum_plays)) * rating \
-        + (minimum_plays / (plays + minimum_plays)) * mean
-    return rating
+    bayes_avg = (rating * plays + minimum_plays * mean / plays + minimum_plays)
+    return bayes_avg
 
 
 def multikeysort(items, columns):
@@ -99,7 +100,7 @@ def get_collection(username):
         title = game['name']['#text'].strip()
         player_rating = game['stats']['rating']['@value']
         plays = game['numplays']
-        rating = weighted_average(player_rating, mean_rating, plays)
+        rating = bayesian_average(player_rating, mean_rating, plays)
         COLLECTION.append({'name': title, 'player_rate': player_rating,
                            'rating': rating, 'plays': plays})
 
@@ -108,20 +109,26 @@ def get_collection(username):
     return collection
 
 
-def display_top_games(collection, count):
+def display_top_games(collection, count, detailed):
     """Display top games based on ratings then number of plays."""
-    print("{0:<5}{1:<7}{2:<9}{3:<7}{4:<100}".format('Rank', 'Rating',
-                                                    'Weighted', 'Plays',
+    if detailed:
+        print("{0:<5}{1:<7}{2:<7}{3:<100}\n".format('Rank', 'Rating', 'Plays',
                                                     'Game'))
+    else:
+        print("{0:<5}{1:<100}\n".format('Rank', 'Game'))
+
     rank = 1
     for game in collection:
-        print(
-            "{0:<5d}{1:<7.1f}{2:<9.4f}{3:<7}{4:<100s}".format(
-                rank, float(
-                    game['player_rate']),float(
-                    game['rating']), int(
-                    game['plays']), str(
-                    game['name']).strip('b').strip('\'').strip('\"')))
+        if detailed:
+            print(
+                "{0:<5d}{1:<7.1f}{2:<7}{3:<100s}".format(
+                    rank, float(
+                        game['player_rate']), int(
+                        game['plays']), str(
+                        game['name']).strip('b').strip('\'').strip('\"')))
+        else:
+            print("{0:<5d}{1:<100s}".format(rank,
+                  game['name'].strip('b').strip('\'').strip('\"')))
         if count:
             if rank < int(count):
                 rank += 1
@@ -133,4 +140,4 @@ def display_top_games(collection, count):
 
 if __name__ == "__main__":
     ARGS = get_args()
-    display_top_games(get_collection(ARGS.user), ARGS.count)
+    display_top_games(get_collection(ARGS.user), ARGS.count, ARGS.detailed)
