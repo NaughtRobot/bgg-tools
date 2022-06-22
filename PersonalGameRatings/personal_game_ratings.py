82f5557b39e8693f67293342b02d2734ceeb28ab
NaughtRobot <naughtrobot@pm.me>
2022-06-21 23:21:50 -0600

    WIP: Impliment Freshness Into Calculations

--- PersonalGameRatings/personal_game_ratings.py
+++ PersonalGameRatings/personal_game_ratings.py
@@ -3,8 +3,10 @@
 """Personal BGG Ratings."""
 
 import argparse
+import math
 import re
 import sys
+from datetime import datetime
 from functools import cmp_to_key
 from operator import itemgetter
 
@@ -27,7 +29,7 @@ def get_args():
         '-v',
         '--version',
         action='version',
-        version='%(prog)s 5.0.0',
+        version='%(prog)s 6.0.0',
         help="Show program's version number")
     parser.add_argument('-u', '--user', help='BGG username',
                         required=True, metavar='')
@@ -43,14 +45,14 @@ def request_data(url):
     requests_cache.install_cache('data_cache')
     while True:
         data = requests.get(url)
-        if not data.status_code == 200 or "try again later" in data.text:
+        if not data.status_code == 200 or data.status_code == 202:
             continue
         else:
             break
     return data.text
 
 
-def bayesian_average(rating, mean, plays):
+def bayesian_average(rating, mean, plays, lastPlayed):
     """Bayesian average for game based on number of plays.
 
     BA = R*(p+m)*M/(p+m)
@@ -62,8 +64,22 @@ def bayesian_average(rating, mean, plays):
     minimum_plays = 5
     rating, mean, plays = float(rating), float(mean), float(plays)
     bayes_avg = (rating * plays + minimum_plays * mean / plays + minimum_plays)
-    return bayes_avg
+    days = datetime.now() - datetime.strptime(lastPlayed, "%Y-%m-%d")
+    days = days.days
+    freshness = bayes_avg/math.log(days+2)
+    return freshness
 
+def get_last_play(username,gameID):
+    """Get last date user played a game."""
+    baseurl = 'https://www.boardgamegeek.com/xmlapi2/'
+    url = baseurl + (f"plays?username={username}&id={gameID}")
+    data = request_data(url)
+    doc = xmltodict.parse(data)
+    try:
+        lastPlayed = doc['plays']['play'][0]['@date']
+    except KeyError:
+        lastPlayed =  doc['plays']['play']['@date']
+    return lastPlayed
 
 def multikeysort(items, columns):
     """Sort dictionary based on multiple keys."""
@@ -99,12 +115,14 @@ def get_collection(username):
     doc = xmltodict.parse(data)
     mean_rating = calculate_mean(doc)
     for game in doc['items']['item']:
+        gameId = game['@objectid']
         title = game['name']['#text'].strip()
         player_rating = game['stats']['rating']['@value']
         plays = game['numplays']
-        rating = bayesian_average(player_rating, mean_rating, plays)
+        lastPlayed = get_last_play(username,gameId)
+        rating = bayesian_average(player_rating, mean_rating, plays, lastPlayed)
         collection.append({'name': title, 'player_rate': player_rating,
-                           'rating': rating, 'plays': plays})
+                           'rating': rating, 'plays': plays, 'last_played': lastPlayed})
 
     collection = multikeysort(collection, ['rating', 'name'])
 
@@ -114,17 +132,17 @@ def get_collection(username):
 def display_top_games(collection, count, detailed):
     """Display top games based on ratings then number of plays."""
     if detailed:
-        print(f"{'Rank':<5}{'Rating':<7}{'Weighted':<10}{'Plays':<7}" \
-              f"{'Game':<100}")
+        print(f"{'Rank':<6}{'Rating':<8}{'Weighted':<11}{'Plays':<7}" \
+              f"{'Last Played':<13}{'Game':<100}")
     else:
         print(f"{'Rank':<5}{'Game':<100}")
     rank = 1
     rgx = re.compile('[%s]' % 'b\'\"')
     for game in collection:
         if detailed:
-            print(f"{rank:<5d}{float(game['player_rate']):<7.1f}" \
-                  f"{float(game['rating']):<10.4f}" \
-                  f"{game['plays']:<7}{rgx.sub('',game['name']):<100s}")
+            print(f"{rank:<6d}{float(game['player_rate']):<8.1f}" \
+                  f"{float(game['rating']):<11.4f}" \
+                  f"{game['plays']:<7}{game['last_played']:<13}{rgx.sub('',game['name']):<100s}")
         else:
             print(f"{rank:<5d}{rgx.sub('',game['name']):<100s}")
         if count:
